a8f5f6a6564cfc251672c8973558b37018e23d0a
lutfianrhdn <lutfian.rhdn@gmail.com>
2025-12-03 20:41:44 +0700

    fix order

--- src/transports/api/validations/order.validation.ts
+++ src/transports/api/validations/order.validation.ts
@@ -9,6 +9,12 @@ export const createOrderSchema = z.object({
       z.object({
         product_id: z.number().int().positive('Product ID must be a positive integer'),
         qty: z.number().int().positive('Quantity must be a positive integer'),
+        product_items_ids: z.array(
+          z.object({
+            product_id:z.number().int().positive('Prooduct ID must be Positive integer'),
+            qty:z.number().int().positive('Quantity must be a Positive Integer'),
+        })
+        )
       })
     ).min(1, 'At least one item is required'),
   }),
